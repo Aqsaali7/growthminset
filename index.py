b1fbfc1c5c534a1fde11b623c9f9da0a4197be5a
Aqsaali7 <aaqsa5411@gmail.com>
2025-04-08 15:00:00 +0500

    index.py

--- index.py
+++ index.py
@@ -53,7 +53,7 @@ if uploaded_files:
                 st.write("Duplicates Removed!")
         with col2:
             if st.button(f"fill missing values for {file.name}"):
-                numeric_cols= df.select_dtypes(includes=['number']).columns
+                numeric_cols= df.select_dtypes(include=['number']).columns
                 df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
                 st.writes("Missing values have been filled!")
         
@@ -77,7 +77,7 @@ if uploaded_files:
                       mime_type = "text/csv"
                     
                   elif conversion_type == "Excel":
-                      df.to.to_excel(buffer, index=False)
+                      df.to_excel(buffer, index=False)
                       file_name= file.name.replace(file_ext, ".xlsx")
                       mime_type ="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                       buffer.seek (0)
